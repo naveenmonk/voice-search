d7902e668e111463bb5316c43d9346a18a29db41
NaveenMONK <naveenbalaji786@gmail.com>
2022-01-05 21:29:07 +0530

    Final version

---
+++ script.js
@@ -0,0 +1,68 @@
+import * as ele from "./utils/elements.js";
+
+const speechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if (speechRecognition) {
+  const recognition = new speechRecognition();
+
+  ele.mic.addEventListener("click", enableMic);
+
+  function enableMic(e) {
+    e.preventDefault();
+    if (ele.mic.classList.contains("fa-microphone")) {
+      recognition.start();
+    } else {
+      recognition.stop();
+    }
+  }
+
+  recognition.addEventListener("start", () => {
+    ele.mic.classList.remove("fa-microphone");
+    ele.mic.classList.add("fa-microphone-slash");
+    ele.instruction.textContent = "Listening....Press ctrl + m to start";
+    ele.searchInput.focus();
+    console.log("Speech is activated !!!");
+  });
+
+  recognition.addEventListener("end", () => {
+    ele.mic.classList.remove("fa-microphone-slash");
+    ele.mic.classList.add("fa-microphone");
+    ele.instruction.textContent = "Press Ctrl + x Click the Mic icon to start";
+    ele.searchInput.focus();
+    console.log("Speech is Deactivated!!!");
+  });
+
+  recognition.continuous = true;
+
+  recognition.addEventListener("result", (e) => {
+    console.log(e);
+    const speech = e.resultIndex;
+    const transcript = e.results[speech][0].transcript;
+    if (transcript.toLowerCase().trim() === "stop recording") {
+      recognition.stop();
+    } else if (!ele.searchInput.value) {
+      ele.searchInput.value = transcript;
+    } else {
+      if (transcript.toLowerCase().trim() === "search") {
+        ele.searchForm.submit();
+      } else if (transcript.toLowerCase().trim() === "reset") {
+        ele.searchInput.value = "";
+      } else {
+        ele.searchInput.value = transcript;
+      }
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key === "x") {
+      recognition.start();
+    }
+    if (e.ctrlKey && e.key === "m") {
+      recognition.stop();
+    }
+  });
+} else {
+  alert("Speech Web api is not supported");
+  ele.speechContainer.style.visibility = "hidden";
+}
